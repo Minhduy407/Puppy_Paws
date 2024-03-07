cf9a2e764a4aef2cfa1ac0e63f78b75a877b68f0
Minhduy407 <minhdii1510@gmail.com>
2024-03-07 15:06:29 +0700

    add lun

--- JS/home.js
+++ JS/home.js
@@ -2,4 +2,20 @@ function toggleList() {
     var list = document.getElementById("list");
     list.style.display = (list.style.display === 'none') ? 'block' : 'none';
 }
+window.addEventListener('scroll', function() {
+    var headerMain = document.getElementById('header__div--main');
+    var headerNav = document.getElementById('header__div--nav');
+    var headerInfo = document.getElementById('header__div--infomation');
+
+    
+    if (window.scrollY > 100) { // You can adjust the scroll threshold as needed
+        headerMain.style.display = 'none';
+        headerInfo.style.display = 'none';
+        headerNav.style.display = 'flex';
+    } else {
+        headerMain.style.display = 'flex';
+        headerNav.style.display = 'flex';
+        headerInfo.style.display = 'flex';
+    }
+});
 
