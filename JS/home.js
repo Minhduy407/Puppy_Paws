10c0c2aee0e5475f52e935985d03102886f294a7
Minhduy407 <minhdii1510@gmail.com>
2024-03-11 23:17:14 +0700

    update

--- JS/home.js
+++ JS/home.js
@@ -6,16 +6,23 @@ window.addEventListener('scroll', function() {
     var headerMain = document.getElementById('header__div--main');
     var headerNav = document.getElementById('header__div--nav');
     var headerInfo = document.getElementById('header__div--infomation');
+    var header = document.getElementById('header');
 
     
     if (window.scrollY > 100) { // You can adjust the scroll threshold as needed
         headerMain.style.display = 'none';
         headerInfo.style.display = 'none';
         headerNav.style.display = 'flex';
+        headerNav.classList.add('header__div--nav');
+        header.classList.add('header__div--nav');
     } else {
         headerMain.style.display = 'flex';
         headerNav.style.display = 'flex';
         headerInfo.style.display = 'flex';
+        headerNav.classList.remove('header__div--nav');
+        header.classList.remove('header__div--nav');
     }
 });
+
+
 
